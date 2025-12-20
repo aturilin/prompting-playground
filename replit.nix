b273b0fa982c5a5804af74087444afa24ddb405a
Japanese Destroyer Kamikaze <sasha.turilin@gmail.com>
2025-12-20 15:16:59 +0100

    Use Nix packages instead of pip

--- replit.nix
+++ replit.nix
@@ -2,6 +2,11 @@
   deps = [
     pkgs.python311
     pkgs.python311Packages.pip
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.httpx
+    pkgs.python311Packages.python-dotenv
+    pkgs.python311Packages.pydantic
     pkgs.nodejs_20
   ];
 }
